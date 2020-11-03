e9334a7adaede2b9f82904454c363f038830e2c7
Marv <marvin.trijasse@gmail.com>
2020-11-03 11:03:03 +0100

    Add main and gitignore

---
+++ main.rb
@@ -0,0 +1,11 @@
+require 'rubyXL'
+
+# texte d'introduction 
+puts "Bienvenue dans Mercure l'outil de gestion des mobilité internationale de l'UGA"
+
+# Etape 1 verification que les voeux sont bien sur des accord UGA
+puts "\t Etape 1: Verification de la conformité des voeux"
+
+
+#Parcours du tableau des étudiants
+
